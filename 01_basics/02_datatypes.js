d4b433b3f0be20d7f71359e978097f0bc525737e
fateh0007 <fatehalam123463@gmail.com>
2024-09-27 12:32:55 +0000

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,11 @@
 //alert(3+3) // we are using node js to run this code 
 //and not browser so it will not run and give error
 
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
