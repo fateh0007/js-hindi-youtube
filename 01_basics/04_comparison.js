523151ae2bd576baed3375484751129a909fba8c
fateh0007 <fatehalam123463@gmail.com>
2024-10-01 13:27:53 +0000

    comparison operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// == checks value
+// === checks value as well as data type (strics checking)
+
+console.log("2" == 2)
+console.log("2" === 2)
