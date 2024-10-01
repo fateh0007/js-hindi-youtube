86aa9258f1fb04c5e5057384e863fc1dd30c9cca
fateh0007 <fatehalam123463@gmail.com>
2024-10-01 13:27:40 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// two types -> Primitive and non - primitive
+
+//Primitive -> call by value (A copy of data will be created)
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference type (Also known as non primitve type)
+// Array, Objects, Functions
+
+const heroes = ["Batman","Superman","Shaktimaan"];
+//object
+let myObj = {
+    name: "Fateh",
+    age: 21
+}
+
+const myFunction = function(){
+    console.log("Hello World!")
+}
