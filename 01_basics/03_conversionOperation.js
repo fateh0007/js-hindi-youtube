1baf65bd531477dc19950624f208abbdcc6716df
fateh0007 <fatehalam123463@gmail.com>
2024-10-01 13:04:38 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,3 +6,16 @@ console.log(typeof (score))
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
+
+//****************Operations************/
+
+let str1 = "Hello "
+let str2 = "Fateh"
+let str3 = str1+ str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2")
+console.log("1"+2+2);
+console.log(1+2+"2") //jo bhi pehle hoga us hi datatype me baakiyon ka conversion ho jayega
+
