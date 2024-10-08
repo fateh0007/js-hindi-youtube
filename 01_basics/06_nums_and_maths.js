031bc3fb53c61f988183677bbfb8e3a5911f655b
fateh0007 <fatehalam123463@gmail.com>
2024-10-08 17:16:15 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,10 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min+1)))
