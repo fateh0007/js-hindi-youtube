1cd2f572205ed12c93f322c11b021a5a39e0dde3
fateh0007 <fatehalam123463@gmail.com>
2024-10-27 11:33:40 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+//Immediately Invoked Function Expression
+
+(function chai(){
+    console.log(`DB Connected`)
+})(); //semicolon lagan jaruri hai end krne k liye
+
+( (sabzi)=>{
+    console.log(`${sabzi} 25 rupya kilo`)
+})("pyaz");
