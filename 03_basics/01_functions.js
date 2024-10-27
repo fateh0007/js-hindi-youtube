fb8237c2c86e4a722507f946967ecb856a4e2e99
fateh0007 <fatehalam123463@gmail.com>
2024-10-27 11:33:20 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+function logIn(username){
+    return `${username} is logged in`
+}
+console.log(logIn("Fateh"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(2,3,4))
